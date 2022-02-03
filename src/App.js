b33abc46328c0d27e4a793f94cbb0a5e211ba60b
Vivian Bow <vivianbao1009@gmail.com>
2022-02-04 03:06:43 +0900

    Created card components, with stying

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from "react"
 import Navbar from "./components/Navbar"
 import Card from "./components/Card"
+import dataSet from "./data.js"
 
 export default function App() {
+  let cardElements = dataSet.map( item => {
+    return(
+      <Card
+        key={item.id}
+        item={item}
+      />
+    )
+  })
   return(
     <>
       <Navbar />
-      <Card />
+      {cardElements}
     </>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,18 @@
 import React from "react"
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <>
-    </>
+    <div className="card-container">
+      <div className="card-image-container"><img className="card-picture" src={`/images/${props.item.img}`} alt={`${props.item.location}`}/></div>
+      <div className="card-content-container">
+        <div className="card-location-sec">
+          <h4 className="location-name"><span className="location-icon"></span>{props.item.location}</h4>
+          <a className="location-link" href={`${props.item.mapLink}`}>View on Google Map</a>
+        </div>
+        <h1 className="card-title">{props.item.title}</h1>
+        <h4 className="card-date">{props.item.startDate} - {props.item.endDate}</h4>
+        <p className="card-description">{props.item.description}</p>
+      </div>
+    </div>
   )
 }
