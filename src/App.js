4919b5ce90310429c637c6b4eef7fc331514b92c
Vivian Bow <vivianbao1009@gmail.com>
2022-02-04 15:57:07 +0900

    Created navbar component with styling

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Navbar from "./components/Navbar"
 import Card from "./components/Card"
 import dataSet from "./data.js"
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faMapMarkerAlt } from '@fortawesome/free-solid-svg-icons'
+import { faMapMarkerAlt, faGlobeAsia} from '@fortawesome/free-solid-svg-icons'
 
-library.add(faMapMarkerAlt)
+library.add(faMapMarkerAlt, faGlobeAsia)
 
 export default function App() {
   let cardElements = dataSet.map( item => {
